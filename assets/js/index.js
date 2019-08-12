c531bc0f5933fa286c247314307e8872e9ce62ea
Dillon Reinwald <dreinwaldmail@gmail.com>
2019-08-12 10:56:32 -0600

    rearranged icons

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,10 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("topButton").style.display = "block";
   } else {
-    document.getElementById("myBtn").style.display = "none";
+    document.getElementById("topButton").style.display = "none";
   }
 }
 
