a61787ddc42650810bdd37cea7ba3c67ed42d6ba
Dillon Reinwald <dreinwaldmail@gmail.com>
2020-01-13 15:55:52 -0700

    switch to jquery, fixed slim link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,39 @@
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("topButton").style.display = "block";
+// function scrollFunction() {
+//   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+//     $("#topButton").style.display = "block";
+//   } else {
+//     $("#topButton").style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+
+
+var btn = $('#topButton');
+
+$(window).scroll(function(){
+  if($(window).scrollTop() > 300){
+      btn.addClass('show');
+      console.log("button showing");
   } else {
-    document.getElementById("topButton").style.display = "none";
+      btn.removeClass('show');
+      console.log("button hide");
   }
-}
+});
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+btn.on('click', function(e) {
+  e.preventDefault();
+  console.log("button clicked");
+  $('html,body').animate({scrollTop:0}, '300');
+});
 
 
 
