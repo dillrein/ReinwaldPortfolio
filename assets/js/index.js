7e4b28ccfeff662cd2d5e95ed25e875e9fa2b8c9
Dillon Reinwald <dreinwaldmail@gmail.com>
2020-07-20 14:30:08 -0600

    added back to top button. adjusted icons sizing and hover effects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,25 @@
 // navbar hide and show
-(function($) {          
-  $(document).ready(function(){                    
-      $(window).scroll(function(){        
-                          //after x distance from top fades in, otherwise fade out 
-          if ($(this).scrollTop() > 400) {
-              $('#Navbar').fadeIn(500);
-          } else {
-              $('#Navbar').fadeOut(500);
-          }
-      });
-  });
-})(jQuery);
+$(document).ready(function () {
+    var btn = $('#BackToTop');
+
+    $(window).scroll(function () {
+        //after x distance from top fades in, otherwise fade out 
+        if ($(this).scrollTop() > 400) {
+            $('#Navbar').fadeIn(500);
+            btn.addClass('show');
+        } else {
+            $('#Navbar').fadeOut(500);
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+
+});
+
+(jQuery);
+
